a144bf3b930e179c829af313f8fb5a64051a5afe
11Rachelle11 <42658403+11Rachelle11@users.noreply.github.com>
2023-03-31 16:23:48 -0500

    backend role code

---
+++ BackendInterface.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public interface BackendInterface {
+    
+    List<MovieInterface> getMoviesByTitle(String title);
+    List<MovieInterface> getMoviesByPopularity(int popularity);
+    List<MovieInterface> getMoviesByYear(int year);
+    List<List<MovieInterface>> compareMoviesByYear(int year1, int year2);
+    List<MovieInterface> getMoviesByYearRange(int year1, int year2);
+    
+    public List<MovieInterface> getMoviesByPopularityRange(int year1, int year2);
+    
+    
+}
