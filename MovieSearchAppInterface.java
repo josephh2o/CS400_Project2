b701522013513bab5714adfede33777e9c1b051a
Joseph <jsho3@wisc.edu>
2023-04-04 22:16:55 -0500

    Update MovieSearchAppInterface.java
    Inserted comments from google docs.

--- MovieSearchAppInterface.java
+++ MovieSearchAppInterface.java
@@ -8,21 +8,21 @@ public interface MovieSearchAppInterface {
 	// prompt for main menu
 	public char mainMenuPrompt();
 
-	// prompt to load data
+	// prompt to load data, calls the backend loadData() method
 	public void loadData();
 
-	// search by title
+	// search by title, calls the backend getMoviesByTitle() method
 	public void searchTitleCommand(List<String> words);
 
-	//search by year
+	//search by year, calls the backend getMoviesByYear() method
 	public void searchYearCommand(List<String> year);
 
-	// search by popularity
+	// search by popularity, calls the backend getMoviesByPopularity() method
 	public void searchPopularityCommand(List<String> words);
 
-	// search by year range
+	// search by year range, calls the backend getMoviesByYearRange() method
 	public void searchYearRange(List<String> rangeMin, List<String>rangeMax);
 
-// search by popularity range
+// search by popularity range, calls the backend getMoviesByPopularityRange() method
 	public void searchPopularityRange(List<String>rangeMin, List<String>rangeMax);
 }
